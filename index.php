a273fb66ca3b62d183ee29af96e25874e51666e8
Philipp <info@atworkz.de>
2017-04-20 16:53:34 +0200

    delete

--- index.php
+++ index.php
@@ -15,10 +15,7 @@ require_once("_config.php");
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
-    <![endif]-->
+
 	<title>&#10084; Lovebox Settings</title>
 	<script src="js/jquery.js"></script>
 	<script src="js/bootstrap.js"></script>
